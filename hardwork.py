004e5e599a84c49a2c706493d7448aac4c569baa
white bear <pavlo.shui2006@gmail.com>
2021-05-08 20:26:47 +0300

    Ended writing code, maybe later adding something

--- hardwork.py
+++ hardwork.py
@@ -4,7 +4,7 @@ from random import randint
 pygame.init()
 
 FPS = 1
-scr = pygame.display.set_mode((1000, 900))
+scr = pygame.display.set_mode((750, 750))
 
 RED = (255, 0, 0)
 BLUE = (0, 0, 255)
@@ -13,38 +13,38 @@ GREEN = (0, 255, 0)
 MAGENTA = (255, 0, 255)
 CYAN = (0, 255, 255)
 BLACK = (0, 0, 0)
-COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN, BLACK]
+COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN]
 
 
 def new_ball():
     """рисует новый шар"""
     global x, y, r
-    x = randint(100, 1000)
-    y = randint(100, 900)
+    x = randint(100, 750)
+    y = randint(100, 750)
     r = randint(10, 100)
     color = COLORS[randint(0, 5)]
     circle(scr, color, (x, y), r)
-    click(event)
-
-
-def click(event):
-    print(x, y, r)
 
 
 pygame.display.update()
 clock = pygame.time.Clock()
 finished = False
-
+results = 0
 while not finished:
     clock.tick(FPS)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             finished = True
         elif event.type == pygame.MOUSEBUTTONDOWN:
-            pos = pygame.MOUSEMOTION
-
-            print("нажата,", pos)
-
+            pos1, pos2 = pygame.mouse.get_pos()
+            if (pos1 >= x - r) and (pos1 <= x + r) and (pos2 >= y - r) and (pos2 <= y + r):
+                results += 1
+                print(results)
+                if results >= 50:
+                    FPS = 2
+                elif results == 30:
+                    print("Через 20 очков скорость поднимится в два раза")
+                    pygame.time.wait(200)
     new_ball()
     pygame.display.update()
     scr.fill(BLACK)
