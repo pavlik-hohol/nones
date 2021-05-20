2fa3e2baa1baa99a220d5f1813f95270837b55dd
pavlik-hohol <pavlo.shui2006@gmail.com>
2021-05-20 13:14:14 +0300

    soso working

--- hardwork.py
+++ hardwork.py
@@ -6,7 +6,7 @@ pygame.init()
 posx = 750
 posy = 750
 
-FPS = 75
+FPS = 50
 scr = pygame.display.set_mode((posx, posy))
 
 WHITE = (255, 255, 255)
@@ -18,10 +18,10 @@ MAGENTA = (255, 0, 255)
 CYAN = (0, 255, 255)
 BLACK = (0, 0, 0)
 COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN]
-
 x = randint(0, posx)
 y = randint(0, posy)
-r = 20
+x1, y1 = randint(0, posx), randint(0, posy)
+r = 35
 color = COLORS[randint(0, 5)]
 
 clock = pygame.time.Clock()
@@ -32,18 +32,37 @@ while not finished:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             finished = True
-        keys = pygame.key.get_pressed()
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1:
+                event.pos1 = event.pos[0]
+                event.pos2 = event.pos[1]
+                if event.pos1 >= x - r and event.pos1 <= x + r and event.pos2 >= y - r and event.pos2 <= y + r and event.pos1 >= x1 - r and event.pos1 <= x1 + r and event.pos2 >= y1 - r and event.pos2 <= y1 + r:
+                    print("results")
+                    results += 1
+        if x < posx and y < posy:
+            x += 5
+            y += 5
+            if y >= posy:
+                y = 0
+                x = 0
+            if x >= posx:
+                x = 0
+                y = 0
+        if x1 < posx and y1 < posy:
+            x1 += 5
+            y1 -= 5
+            if y1 >= posy:
+                y1 = posy - 1
+                x1 = 0
+            if x1 >= posx:
+                x1 = 0
+                y1 = posy - 1
 
-        if keys[pygame.K_UP]:
-            y -= 20
-        elif keys[pygame.K_DOWN]:
-            y += 20
-        elif keys[pygame.K_LEFT]:
-            x -= 20
-        elif keys[pygame.K_RIGHT]:
-            x += 20
+
+
+
     pygame.display.update()
     scr.fill(BLACK)
-    circle(scr, color, (x, y,), r)
-
+    circle(scr, COLORS[randint(5, 5)], (x1, y1), r)
+    circle(scr, color, (x, y), r)
 pygame.quit()
